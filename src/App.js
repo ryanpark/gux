3f7fb0f3eea071143b56704400218fc738b31752
Ryan Park <ryan.park@transport.nsw.gov.au>
2022-05-14 17:57:13 +1000

    finger crossed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import dtipDweb from './images/dtipDweb.png';
 import dtipMweb from './images/dtipMweb.png';
 import itrcDweb from './images/itrcDweb.png';
 import buttons from './images/button.png';
+import gtau from './images/gumtree.png';
+
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<p>Ryan Park</p>
-				<img src={dtipMweb} alt='logo' />
+				<p>
+					G'day, I'm Ryan Park 👋 <br />
+					I'm a UI Designer & Developer Sydney, Australia.
+					<br /> I'm passionate about User Experience, and building products from concept to delivery.
+				</p>
+				<div className='flex-container'>
+					<img src={dtipMweb} alt='logo' />
+					<img src={dtipDweb} alt='logo' />
+				</div>
+
 				<p></p>
-				<img src={dtipDweb} alt='logo' />
+
 				<img src={itrcDweb} alt='logo' />
 				<img src={buttons} alt='logo' />
+				<p></p>
+				<img src={gtau} alt='gumtree' />
+				<p></p>
 			</header>
 		</div>
 	);
